b1e73b0154e27e95d99f1d07c47754bd454ecab2
SepehrRG <srgir04@gmail.com>
2023-03-30 03:57:11 +0430

    Gene part completed

--- Cell.cpp
+++ Cell.cpp
@@ -1,4 +1,7 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<vector>
+
 using namespace std;
 typedef string str;
 #define pb push_back
@@ -12,15 +15,8 @@ protected:
     string DNA[2];
     int RNA_SIZE=RNA.size();
 public:
-    Genome(){
-        RNA = "";
-        DNA[2] = {"",""};
-        RNA_SIZE=0;
-    }
-    Genome(string _RNA, string* _DNA){
-        RNA=_RNA; RNA_SIZE=RNA.size(); 
-        DNA[0]=_DNA[0];
-        DNA[1]=_DNA[1];
+    Genome(string _RNA){
+        RNA = _RNA;
     }
     char comp(char c){
         switch (c)
@@ -41,12 +37,8 @@ public:
         return '?';
     }
     void set_RNA(string _RNA){
-        RNA=_RNA; RNA_SIZE=RNA.size(); 
-    }
-    void set_DNA(string* _DNA){
-        DNA[0]=_DNA[0];
-        DNA[1]=_DNA[1];  
-        RNA_SIZE=DNA[0].size();     
+        RNA=_RNA; 
+        RNA_SIZE=RNA.length(); 
     }
     str get_RNA(){
         return RNA;
@@ -59,19 +51,17 @@ public:
     }
     str complete(str s){
         str new_s="";
-        for(char i:s){
-            new_s+=comp(i);
+        fr( i, RNA_SIZE){
+            new_s+=comp(s[i]);
         }
         return new_s;
     }
-    void make_and_print_complement(){
-        cout<<"complement is :\n";
-        DNA[0]=RNA;DNA[1]="";
-        fr(i,RNA_SIZE){
-            DNA[1]+=comp(RNA[i]);
-            cout<<DNA[1][i];
-        }
-        cout<<"___________";
+    void set_DNA(){
+        DNA[0]= RNA;
+        DNA[1]=complete(RNA);  
+    }
+    void print_complement(){
+        cout<<"complement is :\n"<<DNA[0]<<endl<<DNA[1]<<endl<<"___________";
     }
     void small_mutation(char c, char c1, int n){
         int number=0;
@@ -105,91 +95,49 @@ public:
         cout<<"DNA is :\n"<<DNA[0]<<"\n"<<DNA[1]<<"\n\n";
         cout<<"___________\n";
     }
-    //a new function for KMP algorithm :
-    vector<int>longest_prefix_also_sufix(str s){
-        int n=s.size();
-        int first=0; int second=1;
-        int till_now=0;
-        vector<int>lps(n);
-        while(second<n){
-            if(s[first]==s[second]){
-                till_now++;
-                lps[second] =till_now;
-                first++;second++;
-            }
-            else{
-                lps[second]=0;
-                first=0; second++;
-            }
-        }
-        return lps;
-    }
         void large_mutation(str pat,str exchange){
-        vector<int>lps=longest_prefix_also_sufix(pat); 
-        int M = pat.size();
-        int N = RNA.size();
-        int ind_found;
-        int i0 = 0; // index for DNA[0]
-        int i1 = 0; // index for DNA[1]
-        int j0 = 0; // index for pat[] in DNA[0]
-        int j1 = 0; // index for pat[] in DNA[1]
-        int found_in_wich_string;
-        while ((N - i0) >= (M - j0) && (N - i1) >= (M - j1)) {
-            //for DNA[0]:
-            if (pat[j0] == DNA[0][i0]) {
-                j0++;
-                i0++;
+            int q, i, index=0, second[2];
+            q=pat.length();
+            second[0]=0;
+            second[1]=0;
+            for(i=1;i<q;i++)
+            {
+                if(pat[i]==pat[second[0]])
+                    second[0]++;
+                else if(second[0]>second[1])
+                {
+                    second[1]=second[0];//tedad horof dorost
+                    second[0]=0;
+                    index=i;
+                } 
             }
-            if (j0 == M) {
-                ind_found=i0 - j0;
-                j0 = lps[j0 - 1];
-                found_in_wich_string=0;
-                //if you want to change all paterns remove this break:
-                break;
-            }
-            else if (i0 < N && pat[j0] != DNA[0][i0]) {
-                if (j0 != 0)
-                    j0 = lps[j0 - 1];
+            if(index>=q-2)
+                second[0]= index;//payan akharin harf dorost dovom
+            else
+                second[0]=index+8;
+            index =0;
+            for(i=0; i<get_RNA_size(); i++)
+            {
+                if(index==q)
+                    break;
+                if(RNA[i]==pat[index])
+                    index++;
                 else
-                    i0 = i0 + 1;
+                {
+                    if(index>=second[0])
+                    {
+                        i=i-index+second[0]-1;
+                        index=second[1];
+                    }
+                    else
+                        index=0;
+                }
             }
-            ///for DNA[1]:
-            if (pat[j1] == DNA[1][i1]) {
-                j1++;
-                i1++;
-            }
-            if (j1 == M) {
-                ind_found=i1 - j1;
-                j1 = lps[j1 - 1];
-                found_in_wich_string=1;
-                //if you want to change all paterns remove this break:
-                break;
-            }
-            else if (i1 < N && pat[j1] != DNA[1][i1]) {
-                if (j1 != 0)
-                    j1 = lps[j1 - 1];
-                else
-                    i1 = i1 + 1;
-            }
+            if(index==q)
+                for( ; q>-1;q--)
+                    DNA[1][i-1-q]=comp(DNA[0][i-1-q]);
         }
-        cout<<"founded in index:"<<ind_found<<endl;
-        if(found_in_wich_string==1){
-            DNA[1]=DNA[1].substr(0,ind_found)+exchange+DNA[1].substr(ind_found,DNA[1].size()-1);
-            DNA[0]=DNA[0].substr(0,ind_found)+complete(exchange)+DNA[0].substr(ind_found,DNA[0].size()-1);
-        }
-        else{
-            DNA[1]=DNA[1].substr(0,ind_found)+complete(exchange)+DNA[1].substr(ind_found,DNA[1].size()-1);
-            DNA[0]=DNA[0].substr(0,ind_found)+exchange+DNA[0].substr(ind_found,DNA[0].size()-1);
-        }
-        RNA=DNA[0];
-        cout<<"after large_mutation ";
-        cout<<"DNA is :\n"<<DNA[0]<<"\n"<<DNA[1]<<"\n";
-        cout<<"___________\n";
-    }
-    void reverse_mutation(str s){
-        str rev=s; reverse(rev.begin(),rev.end());
-        large_mutation(s,rev);
-    }
+        
 };
 
 ///////////////////////////////////////
@@ -224,60 +172,99 @@ class cell:public Genome{
             }
             return true;
         }
-        // void small_mutation_chromosomes(str s1, int n,str s2, int m){
-        //     small_mutation()
-        // }
-        // void large_mutation_chromosomes(str s1){
-        // }
-        // void find_all_pallindromes(){
-        //     string RNA=chromosomes[0].get_RNA();
-        //     int n=chromosomes[0].get_RNA_SIZE();
-        //     int index[3];
-        //     index[2]=1;
-        //     index[0]=index[2];
-        //     for( ; index[0]<n; index[0]++)
-        //     {
-        //         cout<<endl<<index[0]<<":";
-        //         if(RNA[index[0]]==comp(RNA[index[0]+1]))
-        //             {
-        //                 cout<<"If "; 
-        //                 index[2]=index[0]+2;
-        //                 index[1]=index[0]-(index[2]-index[0])+1;
-        //                 for( ; index[1]>= 0 && index[2]<n;)
-        //                 {
-        //                     cout<< index[2]<< " "<< index[1]<<" //";
-        //                     if(RNA[index[1]]!=comp(RNA[index[2]]))
-        //                     {
-        //                         cout<<"Break\n";
-        //                         break;
-        //                     }
-        //                     if(index[2]-index[1]>=3)
-        //                     {
-        //                         for(int i=index[1] ; i<=index[2]; i++)
-        //                             cout<<RNA[i];
-        //                         cout<<endl;
-        //                     }
-        //                     index[2]++;
-        //                     index[1]=index[0]-(index[2]-index[0])+1;
-        //                 }
-        //             }
-        //     }
-        // }
-
+        void small_mutation_chromosomes(char s1, char s2, int n, int m){
+            small_mutation();
+        }
+        void large_mutation_chromosomes(str s1){
+        }
+        void find_all_pallindromes(){
+            string RNA=chromosomes[0].get_RNA();
+            int n=chromosomes[0].get_RNA_SIZE();
+            int index[3];
+            index[2]=1;
+            index[0]=index[2];
+            for( ; index[0]<n; index[0]++)
+            {
+                cout<<endl<<index[0]<<":";
+                if(RNA[index[0]]==comp(RNA[index[0]+1]))
+                    {
+                        cout<<"If "; 
+                        index[2]=index[0]+2;
+                        index[1]=index[0]-(index[2]-index[0])+1;
+                        for( ; index[1]>= 0 && index[2]<n;)
+                        {
+                            cout<< index[2]<< " "<< index[1]<<" //";
+                            if(RNA[index[1]]!=comp(RNA[index[2]]))
+                            {
+                                cout<<"Break\n";
+                                break;
+                            }
+                            if(index[2]-index[1]>=3)
+                            {
+                                for(int i=index[1] ; i<=index[2]; i++)
+                                    cout<<RNA[i];
+                                cout<<endl;
+                            }
+                            index[2]++;
+                            index[1]=index[0]-(index[2]-index[0])+1;
+                        }
+                    }
+            }
+        }
 };
 //////////////////////////////////////
 
 int main(){
     cout<<"___________start_____________\n";
-    
+    /*
     //example of genomeL
     str RNA="AAGTCTCTTC";
     str DNA[2]={"AAGTCTCTTC","TTCAGAGAAG"};
     Genome g(RNA,DNA);
     g.large_mutation("TTC","CCC");
+    */
+    str RNA, DNA[2];
+    int n;
+    cout<<"Welcome to our not AI program.\n Please insert RNA.\n";
+    cin>> RNA;
+    cin>> DNA[0]>> DNA[1];
+    Genome g(RNA);//DNA ro k ghaedatan nabayad bedan k, bayad besazim
+    cell c();
+    cout<<"What to you want to do? Don't be shy, mesle barname khodetone.\n1.Gene: Small mutation    2.Gene: Large mutation    3.Gene: Reverse mutation\n4.Cell: Cell death    5.Cell: Large mutation    6.Cell: Reverse mutation    7.Cell: Fine palindormes";
+    again:
+    cin>> n;
+    switch(n)
+    {
+        case 1:
+
+        break;
+        case 2:
+
+        break;
+        case 3:
+
+        break;
+        case 4:
+
+        break;
+        case 5:
+
+        break;
+        case 6:
+
+        break;
+        case 7:
+
+        break;
+        default:
+            cout<<"Dash doroste ke goftam rahat bash, vali hadaghal ye farmani bede ke mojod bashe\ntry again\n";
+            goto again;
+    }
+
+
+
+//
 
 
     //example of cell:
-    cell c;
-
 }
